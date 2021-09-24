c1af670b9bd91446ac0967fbd8315fd0c18c8e74
vinicius <vvenanccio@gmail.com>
2021-09-24 11:21:58 -0300

    Novo Exercicio 03 ponto flutuante

--- Exercicios_Python_hard_way/ex03_with_ponto_flutuante.py
+++ Exercicios_Python_hard_way/ex03_with_ponto_flutuante.py
@@ -4,21 +4,27 @@
 print(" I will now count my chickens:")
 
 valor = (25 + (30/6))
-
-print("meu nome é hens",str(valor))
-
-print("Roster,",str((100-((25*3))%4))) # primeiro * depois % depois subtração
+print(float(valor))
+print("\n")
+print("meu nome é hens",str(float(valor)))
+print("\n")
+print("Roster,",str(float((100-((25*3))%4)))) # primeiro * depois % depois subtração
 #teste do comit
 print( " Agora vamos contar ovos")
-
+print("\n")
 print( (3+2+1-5)+(4%2)-(1/4)+6)
-
+print("\n")
 print("Isso é verdade 3+2<5-7 ?")
 print(3+2<5-7)
-
-print( "what is 3+2?", str(3+2))
-print(" what is 5-7", str(5-7))
+print("\n")
+print("what is 3+2? = ", str(3+2))
+print("\n")
+print("what is 5-7? = ", str(5-7))
+print("\n")
+print("\n")
 print("ohhhh, isso pq isso é falso") ## fui traduzir ficou uma bosta aushauhsuash
-
+print("\n")
 print( "is it greater?", str(5>-2))
-print("isso é menor ou igual ? ", str(5<= -2))
+print("\n")
+print("isso é menor ou igual 5<= -2 ?", str(5<= -2))
+print("\n")

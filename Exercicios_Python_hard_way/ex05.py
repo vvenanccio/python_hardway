c8cf1a96be11ae94811b224b1d2f9462c1dc39fd
vinicius <vvenanccio@gmail.com>
2021-09-27 19:28:01 -0300

    Ex 05

---
+++ Exercicios_Python_hard_way/ex05.py
@@ -0,0 +1,19 @@
+#Strings de formatação
+
+name = 'Vinicius Venancio'
+age = "25" # nao é mentira
+age = 25
+heigth = 1.75 #metros
+weight = 82 #kg
+eyes = "Green"
+teeth = "White"
+cabelo = "Blond"
+
+print(f"Diga um pouco mais sobre vc {name}")
+print(f" Meu nome é {name}")
+print(f"Tenho {weight} quilos e {heigth} metros de altura ")
+print("\n")
+total = age + heigth + weight
+print(total)
+print("\n")
+print(f" Se add minha idade {age}, {heigth} e {weight} I tenho o total de : {total}")

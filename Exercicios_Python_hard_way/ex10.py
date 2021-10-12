fc2efe23e3ce7cb2878f52e8a3875e39bfa354f7
vinicius <vvenanccio@gmail.com>
2021-10-12 16:35:14 -0300

    Ex 10

---
+++ Exercicios_Python_hard_way/ex10.py
@@ -0,0 +1,19 @@
+### aqui o exercicio tava chato e mudei um pouco pra ficar mais legal 
+
+
+var = "\tEu sou o vinicius venancio"
+pessoal = "\u0061"
+letra = "\U00000128"
+
+print(var)
+print(pessoal)
+print(letra)
+
+
+#### aqui o foco era mostrar o uso de escape sequence ou sequencia de escape que nada mais sao do que substituiçoes de strings 
+
+
+### no caso abaixo vou substituir uma string por aspas simples 
+
+## vou substituir o 's por aspas simples
+print("Hello What\'s upp my friend")

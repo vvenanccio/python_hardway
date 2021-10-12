fff82b5e571e50dde508b76098146cea8856f665
vinicius <vvenanccio@gmail.com>
2021-10-12 17:04:10 -0300

    Ex 12

---
+++ Exercicios_Python_hard_way/ex12.py
@@ -0,0 +1,26 @@
+### Ainda sobre inputs 
+
+#idade = input("Digite sua idade")
+#
+#nome = (input("Digite seu nome"))
+#
+#print(f"Seu nome é : {nome}, e dua idade é {idade}")
+
+
+### agora vamos fazer umas contas 
+
+## vamos calcular seu IMC indice de massa corporal
+
+idade_nova = int(input("Digite sua idade: "))
+
+altura = (float(input("Digite sua altura: ")))
+
+peso = (float(input("Digite seu peso: ")))
+
+
+imc = (peso)/(altura * altura)
+
+
+
+print(f"Seu imc é igual a : {round(imc, 2)}")# relembrando o uso do arrendodamento 
+

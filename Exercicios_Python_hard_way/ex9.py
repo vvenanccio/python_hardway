dd963d9a4c1df16d476a94f561261bcce618184b
vinicius <vvenanccio@gmail.com>
2021-10-12 16:15:54 -0300

    Ex 09

---
+++ Exercicios_Python_hard_way/ex9.py
@@ -0,0 +1,14 @@
+# aqui temos uma coisa estranha e nova , digite corretamente 
+
+"\n"# pulador de linhas
+
+
+days = "Segunda terça quarta quinta sexta sabado dominguera"
+
+meses = "janeiro\nfevereiro\nmaço\abril\nmaio\njunho\njulho\nagosto\nsetembro\noutrubro\nnovembro\ndezembro"
+
+print("aqui sao os dias: ", days)
+print("\n")
+print("aqui sao os meses", meses)
+
+

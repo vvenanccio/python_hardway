ce272e51e085d3b3c5c89c6259ab2a13842b1bd4
vinicius <vvenanccio@gmail.com>
2021-10-16 07:09:14 -0300

    Ex 15

---
+++ Exercicios_Python_hard_way/ex15.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(f"Templates/{filename}")
+
+print(txt.read())
+
+print("Digite o nome do arquivo de novo")
+
+file_again = input("> ")
+
+txt_again = open(f"Templates/{file_again}")
+
+print(txt_again.read())
